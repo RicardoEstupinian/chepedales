45085da610b61c1f6dcf79e3bf15006163b90eb0
EdwinPalacio5 <ap16014@ues.edu.sv>
2018-10-27 11:47:32 -0600

    creacion de modelos, carpeta apps y app mainCatalogo

---
+++ chepedales/Apps/mainCatalogo/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Efecto(models.Model):
+	nombre_efecto = models.CharField(max_length=50)
+	descripcion_efecto = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.nombre_efecto
+
+class PublicacionPedal(models.Model):
+	efecto = models.ForeignKey(Efecto, null=True, blank=True, on_delete=models.PROTECT)
+	nombre_pedal = models.CharField(max_length=50)
+	marca = models.CharField(max_length=50)
+	modelo = models.CharField(max_length=50)
+	descripcion_pedal = models.CharField(max_length=1000)
+	precio = models.FloatField()
+	imgPedal = models.ImageField(upload_to='static/img', blank=True)
+	urlVideo = models.CharField(max_length=50)
+	puntuacion = models.FloatField()
+	precio = models.FloatField()
+	fecha_publicacion = models.DateField()
+	autor_publicacion = models.CharField(max_length=50)
+	evaluada = models.BooleanField(default=False)
+	aprobada = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.nombre_pedal
+
+
+
+
