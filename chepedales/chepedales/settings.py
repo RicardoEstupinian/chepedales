5bb5107ef9ddc41ca6ce7a110e5374afac2f1ca1
Ricardo Estupinian <ricaeslo@gmail.com>
2018-10-28 16:01:32 -0600

    Creacion de la app login, configuracion de su url y creacion del template para login, se agregaron otras modificacion al base

--- chepedales/chepedales/settings.py
+++ chepedales/chepedales/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Apps.mainCatalogo',
+    'Apps.login',
 ]
 
 MIDDLEWARE = [
--- chepedales/chepedales/views.py
+++ chepedales/chepedales/views.py
@@ -1,5 +1,2 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def base(request):
-    return render(request,'base/base.html')
