28d84170a786a728d6ae33ae69623bf0f3f6505e
Ricardo Estupinian <ricaeslo@gmail.com>
2018-10-29 23:53:21 -0600

    Merge con cambios realizados por los desarrolladores

--- chepedales/Apps/publicacion/urls.py
+++ chepedales/Apps/publicacion/urls.py
@@ -5,5 +5,6 @@ app_name = "publicacion"
 
 urlpatterns = [
 	path('registrar/', views.PublicacionCreate.as_view(), name="publicacion_registrar"),
-	path('listar/', views.listar_publicacion, name="listar_publicacion")
+	path('listar/', views.listar_publicacion, name="listar_publicacion"),
+	path('mostrar/<pk>/', views.MostrarPublicacion.as_view(), name="mostrar_publicacion"),
 ]
--- chepedales/chepedales/views.py
+++ chepedales/chepedales/views.py
@@ -1,2 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+def base(request):
+    return render (request, 'base/base.html' )
